482a747b56ef8ef6823f79e4b795cb320b384d92
aneesh072 <aneeshkarki292@gmail.com>
2022-08-30 19:00:23 +1000

    note adding functionality

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
+  const [noteText, setNoteText] = useState('');
+
+  const handleChange = (e) => {
+    setNoteText(e.target.value);
+  };
+
+  const handleSave = () => {
+    handleAddNote(noteText);
+    setNoteText('')
+  };
   return (
     <div className="note new">
-      <textarea rows="8" clos="10" placeholder="Type to add a note"></textarea>
+      <textarea
+        rows="8"
+        clos="10"
+        placeholder="Type to add a note"
+        value={noteText}
+        onChange={handleChange}
+      ></textarea>
       <div className="note-footer">
         <small>200 Remaining</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSave}>
+          Save
+        </button>
       </div>
     </div>
   );
