bc953190ac027fcb30f782696296073db62c81cf
aneesh072 <aneeshkarki292@gmail.com>
2022-08-30 20:28:47 +1000

    search functionality added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState('');
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -39,9 +41,11 @@ const App = () => {
   };
   return (
     <div className="container">
-      <Search />
+      <Search handleSearchNote={setSearchText} />
       <NotesList
-        notes={notes}
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
         handleAddNote={addNote}
         handleDeleteNote={deleteNote}
       />
